19e3c4d07940a9cf69ebd35cbbcc533dcdc82d0b
Totori <290Livermorium@gmail.com>
2021-01-09 12:42:40 +0900

    デプロイ時にキャッシュをinvalidationする行を追加

--- deploy_production.bat
+++ deploy_production.bat
@@ -5,3 +5,4 @@ call sam build --config-env staging
 call sam deploy --config-env staging
 cd ..
 aws s3 sync web s3://prod-md-memo.tori-blog.net --delete
+aws cloudfront create-invalidation --distribution-id E2KO2XM60TVSA0 --paths "/*"
--- deploy_staging.bat
+++ deploy_staging.bat
@@ -5,3 +5,4 @@ call sam build --config-env staging
 call sam deploy --config-env staging
 cd ..
 aws s3 sync web s3://stg-md-memo.tori-blog.net --delete
+aws cloudfront create-invalidation --distribution-id EWVQGO1B7LR9X --paths "/*"
