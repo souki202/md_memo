30bac03076c4b620cabab4949ab75a3fdae4204c
Totori <290Livermorium@gmail.com>
2021-01-12 18:55:39 +0900

    サイドバーを作成

--- web/js/auth.js
+++ web/js/auth.js
@@ -14,15 +14,9 @@ new Vue({
     data: () => {
         return {
             isLogin: false,
-            userViewName: '',
-            userData: null,
         }
     },
     mounted() {
-        const viewNameCache = Cookies.get('view_name_cache');
-        if (viewNameCache) {
-            this.userViewName = viewNameCache;
-        }
         this.checkLoggedIn();
     },
     methods: {
@@ -30,10 +24,6 @@ new Vue({
             instance.post(getApiUrl() + '/check_token').then((res) => {
                 console.log('token check success');
                 this.isLogin = true;
-                try {
-                    this.getUserData();
-                } catch (e) {
-                }
             }).catch((err) => {
                 // シェアされたメモを見る場合は, 非ログイン状態でも見れる設定があるので確認
                 const shareId = urlParameter('share_id');
@@ -43,22 +33,6 @@ new Vue({
                     console.log(shareId)
                     location.href = '/login.html';
                 }
-            }).then(() => {
-                // always executed
-            })
-        },
-        getUserData() {
-            instance.get(getApiUrl() + '/get_user_data').then((res) => {
-                console.log(res);
-                this.userData = res.data.user;
-                window.userData = res.data.user;
-                const userId = res.data.user.user_id;
-                this.userViewName = userId.substring(0, userId.indexOf('@'));
-                Cookies.set('view_name_cache', this.userViewName, { domain: document.domain, expires: new Date('1 Jan 2037 00:00:00 GMT') });
-
-            }).catch((err) => {
-                console.log(err);
-                this.userViewName = 'エラーが発生しました'
             }).then(() => {
                 // always executed
             })
@@ -75,3 +49,49 @@ new Vue({
         }
     }
 });
+
+new Vue({
+    el: "#sidebar",
+    data: () => {
+        return {
+            userViewName: '',
+            userGroupName: 'Free plan',
+            userData: null,
+        }
+    },
+    mounted() {
+        const viewNameCache = Cookies.get('view_name_cache');
+        if (viewNameCache) {
+            this.userViewName = viewNameCache;
+        }
+        this.getUserData();
+    },
+    methods: {
+        getUserData() {
+            instance.get(getApiUrl() + '/get_user_data').then((res) => {
+                console.log(res);
+                this.userData = res.data.user;
+                window.userData = res.data.user;
+                const userId = res.data.user.user_id;
+                this.userViewName = userId.substring(0, userId.indexOf('@'));
+                Cookies.set('view_name_cache', this.userViewName, { domain: document.domain, expires: new Date('1 Jan 2037 00:00:00 GMT') });
+            }).catch((err) => {
+                console.log(err);
+                this.userViewName = 'エラーが発生しました'
+            }).then(() => {
+                // always executed
+            })
+        },
+        
+        logout() {
+            axios.post(getApiUrl() + '/logout').then(res => {
+                console.log('success logout');
+            }).catch(err => {
+                console.log('failed to logout on server.');
+            }).then(() => {
+                Cookies.remove('session_token');
+                location.href = '/login.html';                
+            })
+        }
+    }
+})
---
+++ web/js/sidebar.js
@@ -0,0 +1,14 @@
+(() => {
+    const mainDom = document.querySelector('main');
+    document.getElementById("switchSidebar").addEventListener('change', (e) => {
+        const isCheched = e.target.checked;
+        if (isCheched) {
+            mainDom.classList.add('sidebar-opened');
+            mainDom.classList.remove('sidebar-closed');
+        }
+        else {
+            mainDom.classList.remove('sidebar-opened');
+            mainDom.classList.add('sidebar-closed');
+        }
+    });
+})();
